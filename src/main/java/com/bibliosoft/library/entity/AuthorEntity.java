8846b2e7a911d665eef22bfdaa842323062adb7d
Kennet Quiróz <78316880+KennetQ2903@users.noreply.github.com>
2025-09-05 12:04:06 -0600

    fix: increase limit on name prop

--- src/main/java/com/bibliosoft/library/entity/AuthorEntity.java
+++ src/main/java/com/bibliosoft/library/entity/AuthorEntity.java
@@ -29,7 +29,7 @@ public class AuthorEntity {
     @Schema(description = "Identificador único del autor.")
     private Long id;
 
-    @Size(min = 3, max = 20)
+    @Size(min = 3, max = 50)
     @NotBlank
     @Schema(description = "Nombre del autor.")
     private String name;
