391dec3faa04da34dd1f67f71a4b652031b3a45a
Kennet Quiróz <78316880+KennetQ2903@users.noreply.github.com>
2025-09-16 14:54:11 -0600

    fix: conflict with JUnit4 and JUnit5

--- src/test/java/com/bibliosoft/library/entity/AccountEntityTest.java
+++ src/test/java/com/bibliosoft/library/entity/AccountEntityTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class AccountEntityTest {
     @Test
